aac36260dec1462758c4a4ce6b3c2f95b73f11a8
Nikhil96-droid <83655398+Nikhil96-droid@users.noreply.github.com>
2021-10-16 21:05:44 +0530

    Create Square.cpp

---
+++ Square.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+class Square
+{
+    private:
+        return(a*a);
+    }
+    float a;
+    
+    public:
+    float area()
+    {
+    void getdata()
+    {
+        cout<<"Ente the value of a";
+        cin>>a;
+    }
+};
+int main()
+{
+    Square s1;
+    Square s2;
+    Square s3;
+    
+    s1.getdata();
+    s2.getdata();
+    s3.getdata();
+    
+    cout<<s1.area()<<endl;
+    cout<<s2.area()<<endl;
+    cout<<s2.area()<<endl;
+    
+    return 0;
+}
